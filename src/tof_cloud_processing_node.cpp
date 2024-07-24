b3acb37a90edcad6d64f1ae58a14cf12ac9cc883
MACLAB-DIBRIS <francesco95grella@gmail.com>
2024-07-24 19:31:56 +0200

    tested cloud backprojection - single ring

--- src/tof_cloud_processing_node.cpp
+++ src/tof_cloud_processing_node.cpp
@@ -7,11 +7,17 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
-#include "sensor_msgs/point_cloud2_iterator.hpp"
+
 #include "pcl_conversions/pcl_conversions.h"
 #include "pcl/point_cloud.h"
 #include "pcl/point_types.h"
 #include "pcl/filters/passthrough.h"
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 using namespace std::placeholders;
 
@@ -19,7 +25,7 @@ class PointCloudMerger : public rclcpp::Node
 {
 public:
     PointCloudMerger()
-        : Node("point_cloud_merger")
+        : Node("point_cloud_merger"), tf_buffer_(this->get_clock()), tf_listener_(tf_buffer_)
     {
         discoverPointCloudTopics();
 
@@ -44,17 +50,18 @@ private:
             {
                 if (topic_type == "sensor_msgs/msg/PointCloud2" && topic_name != "/merged_point_cloud")
                 {
-                    auto callback = [this, topic_count](const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+                    auto callback = [this, topic_count](const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+                    {
                         this->pointCloudCallback(topic_count, msg);
                     };
                     subscribers_.push_back(create_subscription<sensor_msgs::msg::PointCloud2>(topic_name, 10, callback));
-                    
+
                     // init with empty structured msg
                     auto empty_cloud = std::make_shared<sensor_msgs::msg::PointCloud2>();
                     empty_cloud->height = 1;
                     empty_cloud->width = 0;
                     empty_cloud->is_bigendian = false;
-                    empty_cloud->point_step = sizeof(float) * 3; 
+                    empty_cloud->point_step = sizeof(float) * 3;
                     empty_cloud->row_step = empty_cloud->point_step * empty_cloud->width;
                     empty_cloud->is_dense = false;
                     point_clouds_.push_back(empty_cloud);
@@ -68,7 +75,7 @@ private:
 
     void pointCloudCallback(int index, const sensor_msgs::msg::PointCloud2::SharedPtr msg)
     {
-        //RCLCPP_INFO(get_logger(), "Received point cloud on topic %s", subscribers_[index]->get_topic_name());
+        // RCLCPP_INFO(get_logger(), "Received point cloud on topic %s", subscribers_[index]->get_topic_name());
         point_clouds_[index] = msg;
     }
 
@@ -76,13 +83,22 @@ private:
     {
         std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> pcl_clouds;
         std_msgs::msg::Header header;
+        std::string target_frame = "base_link";
 
         for (const auto &cloud : point_clouds_)
         {
-                pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-                pcl::fromROSMsg(*cloud, *pcl_cloud);
-                pcl_clouds.push_back(pcl_cloud);
-                header = cloud->header;
+            // local point cloud backprojection 
+            sensor_msgs::msg::PointCloud2 transformed_cloud;
+            if (!transformPointCloud(cloud, transformed_cloud, target_frame))
+            {
+                RCLCPP_WARN(this->get_logger(), "Failed to transform point cloud to %s", target_frame.c_str());
+                continue;
+            }
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+            pcl::fromROSMsg(transformed_cloud, *pcl_cloud);
+            pcl_clouds.push_back(pcl_cloud);
+            header = transformed_cloud.header;
         }
 
         std::cout << pcl_clouds.size() << "\n\n";
@@ -103,20 +119,20 @@ private:
         sensor_msgs::msg::PointCloud2 merged_cloud_msg;
         pcl::toROSMsg(*merged_cloud, merged_cloud_msg);
         merged_cloud_msg.header = header; // set back header
-        merged_cloud_msg.header.frame_id = "tof_ring_shoulder_root_link";
+        merged_cloud_msg.header.frame_id = target_frame;
 
         publisher_->publish(merged_cloud_msg);
-        //RCLCPP_INFO(this->get_logger(), "Published merged point cloud");
+        // RCLCPP_INFO(this->get_logger(), "Published merged point cloud");
     }
 
-    bool hasFields(const sensor_msgs::msg::PointCloud2::SharedPtr cloud, const std::vector<std::string>& fields)
+    bool hasFields(const sensor_msgs::msg::PointCloud2::SharedPtr cloud, const std::vector<std::string> &fields)
     {
         std::set<std::string> cloud_fields;
         for (const auto &field : cloud->fields)
         {
             cloud_fields.insert(field.name);
         }
-        
+
         for (const auto &field : fields)
         {
             if (cloud_fields.find(field) == cloud_fields.end())
@@ -127,10 +143,30 @@ private:
         return true;
     }
 
+    bool transformPointCloud(const sensor_msgs::msg::PointCloud2::SharedPtr &input_cloud,
+                            sensor_msgs::msg::PointCloud2 &output_cloud,
+                            const std::string &target_frame)
+    {
+        try
+        {
+            auto transform = tf_buffer_.lookupTransform(target_frame, input_cloud->header.frame_id, tf2::TimePointZero);
+            tf2::doTransform(*input_cloud, output_cloud, transform);
+            return true;
+        }
+        catch (tf2::TransformException &ex)
+        {
+            RCLCPP_WARN(this->get_logger(), "Could not transform %s to %s: %s", input_cloud->header.frame_id.c_str(), target_frame.c_str(), ex.what());
+            return false;
+        }
+    }
+
     std::vector<rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr> subscribers_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
     std::vector<sensor_msgs::msg::PointCloud2::SharedPtr> point_clouds_;
     rclcpp::TimerBase::SharedPtr timer_;
+
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 
 int main(int argc, char *argv[])
